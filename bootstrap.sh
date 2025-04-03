110fb124b22f3c0a7d1959c7d0e9511ed9f05b20
leonardlin <leonardlin@users.noreply.github.com>
2025-04-03 09:32:35 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Starting script"
-sleep 20
+sleep 10
 
 if [ -n "${RUNPOD_POD_ID}" ]; then
     echo "RUNPOD ID: $RUNPOD_POD_ID"
-    /usr/local/bin/runpodctl stop pod $RUNPOD_POD_ID
+    /usr/local/bin/runpodctl remove pod $RUNPOD_POD_ID
 else
   echo "no runpod id set"
 fi
